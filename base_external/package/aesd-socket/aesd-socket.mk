74224ef55d2e99b361081f67c83090d2e21f6eb3
rajshekhar1208 <panjarajshekhar101@gmail.com>
2025-06-30 17:54:57 +0530

    some changes

--- base_external/package/aesd-socket/aesd-socket.mk
+++ base_external/package/aesd-socket/aesd-socket.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_SOCKET_VERSION = 3bcbf5da61de043c5cb5c2f1c39a9ef36b3eb248
+AESD_SOCKET_VERSION = b3621e763d94cf29ae6718a2456d5326c4a2ddab
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -21,6 +21,7 @@ endef
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_SOCKET_INSTALL_TARGET_CMDS
         chmod +x $(@D)/server/aesdsocket-start-stop
+        $(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment5-buildroot/* $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
