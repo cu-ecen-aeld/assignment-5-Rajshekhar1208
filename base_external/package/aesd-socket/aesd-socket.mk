d28e89819c677ae285268694cf0323aea2ae65d3
rajshekhar1208 <panjarajshekhar101@gmail.com>
2025-07-01 17:26:14 +0530

    code fixes

--- base_external/package/aesd-socket/aesd-socket.mk
+++ base_external/package/aesd-socket/aesd-socket.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_SOCKET_VERSION = 88bba30bf447168d7b65e366db2939f4c3438cfe
+AESD_SOCKET_VERSION = 88bba30bf447168d7b65e366db2939f4c3438cf
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
